f12c84bf7186537d25f13e8dd77c6b71c4fecf02
Dynatrace-Asad-Ali <asad.ali@dynatrace.com>
2024-08-14 18:07:16 +0000

    Moved docker run command to post-create

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -1,34 +1,4 @@
 #!/usr/bin/env bash
 
-DT_DEPLOYMENT_URL=${DT_URL}/api/v1/deployment/installer/agent/unix/default/latest?arch=x86
-docker run -d \
---restart=on-failure:5 \
---read-only=true \
---pid=host \
---net=host \
---cap-drop ALL \
---cap-add CHOWN \
---cap-add DAC_OVERRIDE \
---cap-add DAC_READ_SEARCH \
---cap-add FOWNER \
---cap-add FSETID \
---cap-add KILL \
---cap-add NET_ADMIN \
---cap-add NET_RAW \
---cap-add SETFCAP \
---cap-add SETGID \
---cap-add SETUID \
---cap-add SYS_ADMIN \
---cap-add SYS_CHROOT \
---cap-add SYS_PTRACE \
---cap-add SYS_RESOURCE \
---security-opt apparmor:unconfined \
--v /:/mnt/root \
--v /opt:/mnt/volume_storage_mount \
--e ONEAGENT_ENABLE_VOLUME_STORAGE=true \
--e ONEAGENT_INSTALLER_SCRIPT_URL=$DT_DEPLOYMENT_URL \
--e ONEAGENT_INSTALLER_DOWNLOAD_TOKEN=$DT_PAAS_TOKEN\
-dynatrace/oneagent  
-
 #wget  -O Dynatrace-OneAgent-Linux-1.295.66.20240805-161707.sh "$DT_DEPLOYMENT_URL" --header="Authorization: Api-Token $DT_PAAS_TOKEN"
 
